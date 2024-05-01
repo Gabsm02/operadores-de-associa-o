a7fbc4dd6a338b7451eeff62a0c64b946ce5eca9
Gabriel Mendes <gabrielomendes02@gmail.com>
2024-05-01 15:52:17 -0300

    Aprendendo sobre operadores de associação

---
+++ associacao.py
@@ -0,0 +1,6 @@
+#Os operadores de associacao são usados para testar se uma sequencia e apresentada em um objeto.
+
+lista = ['Magalu', "Via", "Carrefour", "Amazon"]
+
+print('Magalu' in lista) # True
+print('Via' not in lista) # False
